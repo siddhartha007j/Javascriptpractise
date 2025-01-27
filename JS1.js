fd7636439e79498cda210e1858bca11749b0a003
siddhartha007j <siddhartha3399@gmail.com>
2025-01-28 03:45:41 +0530

    Javascript practice

---
+++ JS1.js
@@ -0,0 +1,50 @@
+/*
+Variable declaration
+how js run the code  --> dynamically typed language
+*/
+console.log("This is inside JAVA SCRIPT");// for get in console
+/******************************** ***************************************************
+//variable declaration
+let varName;
+// default value of variable id undefined, 
+//currently it holds undefined as value
+
+console.log(varName);
+
+// we can change the value too
+varName=10;
+console.log(varName);
+// strings
+let newVar;
+console.log(newVar);// its undefined
+newVar="string";
+console.log(newVar);
+//boolean
+newVar=true;
+console.log(newVar);
+// it will take the value of last 
+
+//null, undefined (non-existence)
+newVar=null;
+console.log(newVar);*
+********************************************************************************************/
+/**
+ * how to know type of value of a variable
+ **** what type it can have
+ *Primitive
+ --> number, strings, boolean, null, undefined
+ --> bigInt, symbols
+
+ *Non primitive
+    --> functions, arrays and objects
+let varName;
+console.log(typeof varName);
+varName=10;
+console.log(typeof varName);
+varName=10.5;
+console.log(typeof varName);
+varName="New data";
+console.log(typeof varName);
+varName=true;
+console.log(typeof varName);
+*/
